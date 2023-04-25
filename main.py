d60cc99905531e12513645fcd23c3211d3bb17cd
Mohab Ashraf <63811158+MohabASHRAF-byte@users.noreply.github.com>
2023-04-25 13:15:22 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from time import sleep
 with Booking() as bot:
     bot.get_land_page()
     bot.skip_sign_in()
-    bot.choose_currency(currency='EGP')
+    # bot.choose_currency(currency='EGP')
     bot.choose_destinations(destination="hurghada")
-    bot.set_dates(checkIn='2024-01-20', checkOut='2024-01-26')
-    bot.list(5, 1, 2)
+    bot.set_dates(checkIn='2023-04-25', checkOut='2023-04-27')
+    bot.list(2, 0, 1)
     bot.submit()
+    bot.apply_filtrations()
     sleep(10000)
